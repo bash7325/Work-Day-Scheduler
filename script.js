f68c24422e215d0d5fe0958d83835a88b8bcb9b0
Brandon Ashby <bash7325@gmail.com>
2021-01-13 16:20:12 -0700

    added color chage function

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-$(document).ready(function() {
-    //Set current day at top from Moment API
-$("#currentDay").text(moment().format("MMMM Do YYYY"));
-//    
-//color code each block to indicate whether in past, present, future (use classes in CSS)
+$(document).ready(function () {
+  
+    //Variable to store current hour from Moment
+  
+  var currentHour = moment().hours();
+  
+  //Set current day at top from Moment API
+  
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
+  
+  //color code each block to indicate whether in past, present, future (use classes in CSS)
 
-//when text entered into time block, save button saves in local storage
-$(".saveBtn").on("click", function () {
+  function colorChange() {
+    $(".time-block").each(function () {
+      var rowHour = $(this).attr("id");
+      var rowNumber = parseInt(rowHour);
+      if (rowNumber === currentHour) {
+        $(this).addClass("present");
+      } else if (rowNumber < currentHour) {
+        $(this).addClass("past");
+      } else {
+        $(this).addClass("future");
+      }
+      console.log(rowNumber);
+      console.log(rowHour)
+      console.log(currentHour)
+    });
+  }
+
+  colorChange();
+  //when text entered into time block, save button saves in local storage
+
+  $(".saveBtn").on("click", function () {
     var textBox = $(this).siblings(".description").val();
     var timeOfDay = $(this).parent().attr("id");
 
     localStorage.setItem(textBox, timeOfDay);
-})
-//when page is refreshed, saved text is reloaded from local storage
+  });
+  
+  //when page is refreshed, saved text is reloaded to block hours from local storage
 
+  
 });
