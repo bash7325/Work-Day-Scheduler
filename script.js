497c93d7e84724cb06b164aeb4f32a90d7a99686
Brandon Ashby <bash7325@gmail.com>
2021-01-16 13:47:31 -0700

    format broke styling, reverted to old commit

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ $(document).ready(function () {
   //Variable to load/store current hour from Moment
 
   var currentHour = moment().hours();
+  var saveBtn = $(".saveBtn");
 
   //Set current day at top from Moment API
 
   $("#currentDay").text("Today is " + moment().format("MMMM Do YYYY"));
 
-  //color code each block to indicate whether in past, present, future (use classes in CSS)
+  //Color code each block to indicate whether in past, present, future (use classes in CSS)
 
   function colorChange() {
     $(".time-block").each(function () {
       var rowHour = $(this).attr("id");
       var rowNumber = parseInt(rowHour);
-      
+
       if (rowNumber === currentHour) {
         $(this).addClass("present");
       } else if (rowNumber < currentHour) {
@@ -21,22 +22,31 @@ $(document).ready(function () {
       } else {
         $(this).addClass("future");
       }
-      console.log(rowNumber);
-      console.log(rowHour);
-      console.log(currentHour);
     });
   }
 
   colorChange();
-  //when text entered into time block, save button saves in local storage
 
-  $(".saveBtn").on("click", function () {
-    var timeOfDay = $(this).siblings(".description").val();
-    var textBox = $(this).parent().attr("id");
+  //When text entered into time block, save button saves in local storage
 
-    localStorage.setItem(textBox, timeOfDay);
+  saveBtn.on("click", function () {
+    var timeOfDay = $(this).siblings(".hour").text();
+    var task = $(this).siblings(".description").val();
+
+    localStorage.setItem(timeOfDay, task);
   });
 
-  //when page is refreshed, saved text is reloaded to block hours from local storage
+  //When page is refreshed check to see if local storage has anything in it, if it does text is reloaded from local storage
   
+  function saveTask() {
+    $(".hour").each(function () {
+      var currHour = $(this).text();
+      var currTask = localStorage.getItem(currHour);
+
+      if (currTask !== null) {
+        $(this).siblings(".description").val(currTask);
+      }
+    });
+  }
+  saveTask();
 });
